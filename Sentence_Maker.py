c6b0bedc519f8b08100a91cb7835f392ce92afcd
TreacherousToast <callumoriley@gmail.com>
2018-05-08 08:44:53 -0700

    Add files via upload

---
+++ Sentence_Maker.py
@@ -0,0 +1,74 @@
+import random
+import string
+
+contents = []
+print("Enter some sample text (press enter then Ctrl-D to exit): ", end="")
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    contents.append(line)
+
+print("")
+inputString = ""
+
+for i in range(0,len(contents)):
+    inputString = inputString + contents[i]
+punctuationTranslationMap = str.maketrans('', '', string.punctuation)
+extraMap = str.maketrans(' ',' ',"“”‘’")
+numMap = str.maketrans('','',"0123456789")
+
+inputString = inputString.translate(punctuationTranslationMap)
+inputString = inputString.translate(extraMap)
+inputString = inputString.translate(numMap)
+inputString = inputString.replace("  "," ")
+inputString = inputString.replace("   "," ")
+
+inputArr = inputString.lower().split(" ")
+words = []
+followerWords = []
+followerWordNum = []
+
+for i in range(0,len(inputArr)-1):
+    words.append(inputArr[i])
+    followerWords.append(inputArr[i+1])
+    followerWordNum.append(0)
+
+for i in range(0,len(words)):
+    for j in range(0,len(words)):
+        if words[j] == words[i] and followerWords[j] == followerWords[i]:
+            followerWordNum[i] += 1
+
+repeat = [False]*len(words)
+
+for i in range(0,len(words)):
+    if repeat[i] == False:
+        for j in range(0,len(words)):
+            if j != i and words[j] == words[i] and followerWords[j] == followerWords[i] and followerWordNum[j] == followerWordNum[i]:
+                repeat[j] = True
+                
+while True in repeat:
+    index = repeat.index(True)
+    words.pop(index)
+    followerWords.pop(index)
+    followerWordNum.pop(index)
+    repeat.pop(index)
+    
+sentenceLen = int(input("Enter how long you want the sentence to be: "))
+sentence = [words[random.randint(0,len(words))]]
+usedIndices = []
+
+for i in range(1,sentenceLen):
+    currMax = 0
+    currMaxIndex = 0
+    for j in range(0,len(words)):
+        if words[j] == sentence[i-1] and followerWordNum[j] > currMax and j not in usedIndices:
+            currMaxIndex = j
+            currMax = followerWordNum[j]
+
+    sentence.append(followerWords[currMaxIndex])
+    usedIndices.append(currMaxIndex)
+
+for i in range(0, len(sentence)):
+    print(sentence[i],end = " ")
